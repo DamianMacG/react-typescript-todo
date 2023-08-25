e5db5a0507d273cc5a9cefab96c994b34d81a657
Damian <damiangildea12@gmail.com>
2023-08-25 14:20:19 +0100

    Working on dragging functionality

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useRef } from "react";
 
-type InputFieldProps = {
-    todo: string;
-    setTodo: React.Dispatch<React.SetStateAction<string>>;
-    handleAdd: (e: React.FormEvent) => void;
+interface props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<props> = ({ todo, setTodo, handleAdd }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        type="text"
+        placeholder="Enter a Task"
+        value={todo}
+        ref={inputRef}
+        onChange={(e) => setTodo(e.target.value)}
+        className="input__box"
+      />
+      <button type="submit" className="input_submit">
+        GO
+      </button>
+    </form>
+  );
 };
 
-const InputField:React.FC<InputFieldProps> = ({todo, setTodo, handleAdd}) => {
-    
-    return <form className='input-form' onSubmit={(e) => {
-        handleAdd(e)}}>
-        <input type="input" value={todo} onChange={(e) => setTodo(e.target.value)} placeholder='Enter a task' className='input-box'/><br></br>
-        <button className='button' type='submit'>Add Task</button>
-        </form>
-}
 export default InputField;
